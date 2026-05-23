d777d743ebde1c4fd18380039470517605e03e51
IsaacPhenom <isaacboa98@gmail.com>
2026-05-23 20:31:54 +0000

    yeah

--- week03/ScriptureMemorizer/Reference.cs
+++ week03/ScriptureMemorizer/Reference.cs
@@ -11,7 +11,7 @@ public class Reference
         _book = book;
         _chapter = chapter;
         _verse = verse;
-        _endVerse = 0; 
+        _endVerse = 0;
     }
 
     // Constructor for a range of verses
